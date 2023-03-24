2e81fe5df9fcfdb24e8f01cc711e32798fcebb90
Muhammad Noman Aslam Maken <mnomanaslam6@gmail.com>
2023-03-24 22:08:22 +0500

    Insert code update

--- insert_data.php
+++ insert_data.php
@@ -1,43 +1,27 @@
 <?php
 
-function insertIntoTable($tableName, $data) {
-    // Convert data array keys to comma separated column names
+function insert($tableName, $data, $successMsg = 'Data inserted successfully.') {
+    global $connect;
     $columns = implode(", ", array_keys($data));
-    
-    // Build the placeholders for the values
     $placeholders = ":" . implode(", :", array_keys($data));
-    
-    // Build the SQL query
     $sql = "INSERT INTO $tableName ($columns) VALUES ($placeholders)";
-    
-    // Prepare the statement
     $stmt = $connect->prepare($sql);
-    
-    // Bind the parameters
     foreach ($data as $key => $value) {
         $stmt->bindValue(":$key", $value);
     }
-    
-    // Execute the statement
     try {
         $stmt->execute();
-        return "Data inserted successfully.";
+        return $successMsg;
     } catch (PDOException $e) {
         return "Error: " . $e->getMessage();
     }
 }
 
+// Example usage:
+echo insert('coupon', ['coupon' => 'DISCOUNT10', 'status' => 'Pending', 'coupon_discount' => 10]);
+// Output: Data inserted successfully.
 
-//Usage
-
-$data = array(
-    'coupon' => 'COUPONCODE',
-    'status' => 'Pending',
-    'coupon_discount' => 20.00
-);
-
-$result = insertIntoTable('coupon', $data);
-
-echo $result;
+echo insert('coupon', ['coupon' => 'DISCOUNT20', 'status' => 'Approved', 'coupon_discount' => 20], 'Coupon inserted successfully and approved.');
+// Output: Coupon inserted successfully and approved.
 
 ?>
